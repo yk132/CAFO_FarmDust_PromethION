a55dcfae0e533e1859810c8c71f422a5048292a8
yk132 <53902254+yk132@users.noreply.github.com>
2024-05-27 22:03:04 -0400

    change model

--- Job1_download_model.sh
+++ Job1_download_model.sh
@@ -11,19 +11,19 @@ set -u
 export DORADO_SIF_PATH='oras://gitlab-registry.oit.duke.edu/granek-lab/granek-container-images/dorado-simg:0.4.2'
 export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
 export DORADO_MODEL_DIR="${WORK_DIR}/dorado_models" # CHANGE ME 
-export DORADO_941_SUP="dna_r9.4.1_e8_sup@v3.6" # CHANGE ME 
+export DORADO_1041_SUP="dna_r10.4.1_e8.2_400bps_sup@v5.0.0" # CHANGE ME 
 #------------------------
 
 
-if [ -d "${DORADO_MODEL_DIR}/${DORADO_941_SUP}" ]; then
-  echo "${DORADO_MODEL_DIR}/${DORADO_941_SUP} exists."
+if [ -d "${DORADO_1041_SUP}/${DORADO_1041_SUP}" ]; then
+  echo "${DORADO_1041_SUP}/${DORADO_1041_SUP} exists."
 else
-  echo "${DORADO_MODEL_DIR}/${DORADO_941_SUP} does NOT exist. Need to download models"
+  echo "${DORADO_MODEL_DIR}/${DORADO_1041_SUP} does NOT exist. Need to download models"
   mkdir -p $DORADO_MODEL_DIR
 
   singularity exec \
     --bind /work:/work \
     ${DORADO_SIF_PATH} \
-    dorado download --directory $DORADO_MODEL_DIR --model $DORADO_941_SUP
+    dorado download --directory $DORADO_MODEL_DIR --model $DORADO_1041_SUP
 fi
 
