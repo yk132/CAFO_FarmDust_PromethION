4a3202147c75b09dcc8603b8197d04e10336d9ba
Eva Kim <yk132@duke.edu>
2024-05-29 09:36:23 -0400

    demux worked

--- Job3_demux.sh
+++ Job3_demux.sh
@@ -22,7 +22,7 @@ export WORK_DIR="/work/${USER}" # CHANGE ME if needed
 export DORADO_MODEL_DIR="${WORK_DIR}/dorado_models" # CHANGE ME 
 export DORADO_1041_SUP="dna_r10.4.1_e8.2_400bps_sup@v4.2.0" # CHANGE ME 
 export DORADO_RES_DIR="${CAFO_RES_DIR}/Job2_dorado_fastq_res"
-export DORADO_RES_FASTQ="${CAFO_RES_DIR}/duplex_basecalled.fastq"
+export DORADO_RES_FASTQ="${DORADO_RES_DIR}/duplex_basecalled.fastq"
 export DEMUX_RES_DIR="${CAFO_RES_DIR}/Job3_demux_res"
 export NTHREADS="32"
 export KIT_NAME="SQK-RBK114-24"
@@ -48,4 +48,5 @@ singularity exec \
        dorado demux \
        --output-dir ${DEMUX_RES_DIR}  \
        --kit-name ${KIT_NAME} \
-       --emit-fastq 
+       --emit-fastq \
+       ${DORADO_RES_FASTQ} 
