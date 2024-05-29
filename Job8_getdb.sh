07a15f12237053acc719a7442f0b8c0e2c879351
Eva Kim <yk132@duke.edu>
2024-05-29 12:40:03 -0400

    update file for just getting database

--- Job8_getdb.sh
+++ Job8_getdb.sh
@@ -10,22 +10,19 @@
 
 #-------------------------------
 export STORE_DIR="/hpc/home/yk132/storage"
-export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
-export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore_clean"
+export CAFO_DIR="${STORE_DIR}/data/CAFO_FarmDust_PromethION/CAFO_FarmDust/"
+export CAFO_GIT_DIR="${STORE_DIR}/CAFO_FarmDust_PromethION"
+export CAFO_RES_DIR="${CAFO_DIR}/CAFO_PromethION_output"
+export MERGE_RES_DIR="${CAFO_RES_DIR}/Job4_merge_qc_res"
+export FARM_BLANK="${MERGE_RES_DIR}/Blank.fastq"
 export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
 export CAFO_WORK_DIR="${WORK_DIR}/CAFO_Nanopore_work"
 export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
 export NXF_WORK="${CAFO_WORK_DIR}/nf_working"
 export NXF_TEMP="${CAFO_WORK_DIR}/nf_temp"
-export DATA_DIR="${CAFO_DIR}/20230908_1516_MN33275_FAK80584_b5659fa0"
-#export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
-#export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
-export OUTPUT_DIR="${CAFO_DIR}/Outputs"
-export MERGED_DIR="${OUTPUT_DIR}/Job4_merged_fastq"
-export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
-export DB_DIR="${OUTPUT_DIR}/Job8_db"
-export RESULTS_DIR="${OUTPUT_DIR}/Job8_wf_meta_barcode03_res"
-export SLURM_CPUS_PER_TASK="30" # CHANGE ME
+export NTHREADS="32"
+export DB_DIR="${CAFO_RES_DIR}/Job7_Minimap2_db"
+export RESULTS_DIR="${CAFO_RES_DIR}/Job7_blank_wf_metagenomics"
 #-------------------------------
 
 # make directories
@@ -40,10 +37,10 @@ mkdir -p $RESULTS_DIR
 nextflow run epi2me-labs/wf-metagenomics/main.nf \
 	-profile singularity \
 	-c ./nextflow.config \
-	--fastq $BARCODE03_FASTQ \
+	--fastq $FARM_BLANK \
 	--database_set ncbi_16s_18s_28s_ITS \
 	--classifier minimap2 \
-	--threads $SLURM_CPUS_PER_TASK \
+	--threads $NTHREADS \
 	--out_dir $RESULTS_DIR \
 	--min_len 50 \
 	--store_dir $DB_DIR 
--- main_job5_7_contigs.sh
+++ main_job5_7_contigs.sh
@@ -5,8 +5,8 @@ shopt -s nullglob
 
 #-----------------------------
 export STORE_DIR="/hpc/home/yk132/storage"
-export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
-export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore_clean"
+export CAFO_DIR="${STORE_DIR}/data/CAFO_FarmDust_PromethION/CAFO_FarmDust/20240521_1737_P2S-01272-B_PAS35763_a5f7cd95"
+export CAFO_GIT_DIR="${STORE_DIR}/CAFO_FarmDust_PromethION"
 export LOG_DIR="${CAFO_GIT_DIR}/log_dir"
 #-----------------------------
 
