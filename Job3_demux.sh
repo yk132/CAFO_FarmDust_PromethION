4f02e514f3a2704f146040717f776ae83c447600
yk132 <53902254+yk132@users.noreply.github.com>
2024-05-29 09:15:26 -0400

    Update Job3_demux.sh

--- Job3_demux.sh
+++ Job3_demux.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-#SBATCH --partition=dmcshared
+#SBATCH --partition=chsi-gpu
+#SBATCH -A chsi
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=300G
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=0
+#SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 
 set -u
@@ -20,11 +22,21 @@ export WORK_DIR="/work/${USER}" # CHANGE ME if needed
 export DORADO_MODEL_DIR="${WORK_DIR}/dorado_models" # CHANGE ME 
 export DORADO_1041_SUP="dna_r10.4.1_e8.2_400bps_sup@v4.2.0" # CHANGE ME 
 export DORADO_RES_DIR="${CAFO_RES_DIR}/Job2_dorado_fastq_res"
-export DORADO_RES_FASTQ="${CAFO_RES_DIR}/basecalled.fastq"
+export DORADO_RES_FASTQ="${CAFO_RES_DIR}/duplex_basecalled.fastq"
 export DEMUX_RES_DIR="${CAFO_RES_DIR}/Job3_demux_res"
 export NTHREADS="32"
 export KIT_NAME="SQK-RBK114-24"
 #------------------------
+
+#------------------------ from Dr. Granek
+if [ -v SLURM_GPUS_ON_NODE ] && (( $SLURM_GPUS_ON_NODE >= 1 )); then
+  export DORADO_DEVICE="cuda:all" ;
+else 
+  export DORADO_DEVICE="cpu" ;
+fi
+echo $DORADO_DEVICE
+#------------------------
+
 
 mkdir -p $DEMUX_RES_DIR
 
@@ -36,5 +48,4 @@ singularity exec \
        dorado demux \
        --output-dir ${DEMUX_RES_DIR}  \
        --kit-name ${KIT_NAME} \
-       --emit-fastq \
-       --threads ${NTHREADS} 
+       --emit-fastq 
