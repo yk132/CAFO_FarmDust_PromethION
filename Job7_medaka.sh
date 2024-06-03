7bcbb5bf01fcbccfe8da91efca425025c7aae344
yk132 <53902254+yk132@users.noreply.github.com>
2024-06-03 11:44:57 -0400

    medaka instructions differ??

--- Job7_medaka.sh
+++ Job7_medaka.sh
@@ -17,19 +17,19 @@ export STORE_DIR="/hpc/home/yk132/storage"
 export CAFO_DIR="${STORE_DIR}/data/CAFO_FarmDust_PromethION/CAFO_FarmDust/"
 export CAFO_GIT_DIR="${STORE_DIR}/CAFO_FarmDust_PromethION"
 export CAFO_RES_DIR="${CAFO_DIR}/CAFO_PromethION_output"
+export FASTQ_DIR="${CAFO_RES_DIR}/demux_simplex_filtered"
 export FLYE_RES_DIR="${CAFO_RES_DIR}/Job2_flye_res"
 export FLYE_A_DIR="${FLYE_RES_DIR}/FarmA"
 export FLYE_C_DIR="${FLYE_RES_DIR}/FarmC"
 export FLYE_BLANK_DIR="${FLYE_RES_DIR}/Blank"
 export FLYE_UNCLASSIFIED_DIR="${FLYE_RES_DIR}/Unclassified"
-export NTHREADS="16"
 export FARMA_ASSEMBLED="${FLYE_A_DIR}/assembly.fasta"
 export FARMC_ASSEMBLED="${FLYE_C_DIR}/assembly.fasta"
 export BLANK_ASSEMBLED="${FLYE_BLANK_DIR}/assembly.fasta"
 export UNCLASSIFIED_ASSEMBLED="${FLYE_UNCLASSIFIED_DIR}/assembly.fasta"
 # medaka output
-export MEDAKA_RES_DIR="${CAFO_RES_DIR}/Job4_medaka"
-export MEDAKA_FARMA="${QUAST_RES_DIR}/Farm_A"
+export MEDAKA_RES_DIR="${CAFO_RES_DIR}/Job7_medaka"
+export MEDAKA_FARMA="${QUAST_RES_DIR}/Farm_A_polished."
 export MEDAKA_FARMC="${QUAST_RES_DIR}/Farm_C"
 export MEDAKA_BLANK="${QUAST_RES_DIR}/Blank"
 export MEDAKA_UNCLASSIFIED="${QUAST_RES_DIR}/Unclassified"
@@ -53,9 +53,11 @@ mkdir -p $MEDAKA_UNCLASSIFIED
 
 # medaka docker pull nanozoo/medaka:1.11.3--ce388c3
 
-# Run Quast on assembled contigs
+# polish contig with medaka 
 singularity exec \
 	--nv \
 	--bind /work:/work \
 	--bind /hpc/group:/hpc/group \
         docker://nanozoo/medaka:1.11.3--ce388c3 \
+	medaka consensus --auto_model consensus \
+ 	$FARMA_ASSEMBLED -i ${FASTQ_DIR}/Farm_A.fastq.gz -o $MEDAKA_FARMA
